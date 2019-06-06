aa2f91d72b7151ce70796390519266258f0c51eb
Ravik27280 <Ravik27280@gmail.com>
2019-06-06 11:12:37 +0530

    add image target

---
+++ Card/Assets/scripts/WebURLScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebURLScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public void btnOne()
+    {
+        Application.OpenURL("https://www.facebook.com/profile.php?id=100004305275178");
+    }
+
+    public void btnTwo()
+    {
+        Application.OpenURL("www.google.com");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
